c7d954725a5a6686d619de854b4506f22457e48b
Ken Naruo <ken,naruo@gmail.com>
2021-03-20 09:30:27 +0900

    update 20210320

--- dev.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cp /var/www/cgi-bin/book_app /root/dev_book/book_app/cgi-bin/
-cp /var/www/bin/*book* /root/dev_book/book_app/bin/
-cp /var/www/descriptor/*book* /root/dev_book/book_app/descriptor/
