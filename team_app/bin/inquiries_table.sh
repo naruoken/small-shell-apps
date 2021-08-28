18f4f7d141b82bdd846a9cfc53525398a4ef8fdd
Ken Naruo <ken,naruo@gmail.com>
2021-08-28 11:35:00 +0900

    update 20210828

--- team_app/bin/inquiries_table.sh
+++ team_app/bin/inquiries_table.sh
@@ -65,13 +65,15 @@ else
     | sed "s/_/{%%%%%%%}/g" | sed "s/\//{%%%%%}/g"  \
     | sed "s/(/{%%%%%%%%}/g" | sed "s/)/{%%%%%%%%%}/g" | sed "s/\[/{%%%%%%%%%%}/g" | sed "s/\]/{%%%%%%%%%%%}/g" \
     | sed "s/'/{%%%%%%%%%%%%%%%%%}/g" | sed "s/*/{%%%%%%%%%%%%%%%}/g" | sed "s/\\\\$/{%%%%%%%%%%%%%%}/g" \
-    | sed "s/,/{%%%%%%}/g"  | sed "s/#/{%%%%%%%%%%%%%}/g" |  sed "s/\&/{%%%%}/g" | sed "s/:/{%%%}/g" | sed "s/　/ /g" | sed "s/ /,/g"`
+    | sed "s/,/{%%%%%%}/g"  | sed "s/#/{%%%%%%%%%%%%%}/g" |  sed "s/\&/{%%%%}/g" | sed "s/:/{%%%}/g" | sed "s/　/ /g" | sed "s/ /,/g" \
+    |  php -r "echo preg_quote(file_get_contents('php://stdin'));"`
     filter_table="$filter_key{$filter_word}"
   else
     filter_table=`echo $table_command  | sed "s/%/{%%%%%%%%%%%%%%%%}/g" | sed "s/_/{%%%%%%%}/g" | sed "s/\//{%%%%%}/g" | sed "s/,/{%%%%%%}/g" \
     | sed "s/\[/{%%%%%%%%%%}/g" | sed "s/\]/{%%%%%%%%%%%}/g"| sed "s/(/{%%%%%%%%}/g" | sed "s/)/{%%%%%%%%%}/g" | sed "s/|/{%%%%%%%%%%%%}/g" \
     | sed "s/'/{%%%%%%%%%%%%%%%%%}/g" | sed "s/*/{%%%%%%%%%%%%%%%}/g" | sed "s/\\\\$/{%%%%%%%%%%%%%%}/g" \
-    | sed "s/#/{%%%%%%%%%%%%%}/g" |  sed "s/\&/{%%%%}/g" | sed "s/:/{%%%}/g" | sed "s/　/ /g" | sed "s/ /,/g"`
+    | sed "s/#/{%%%%%%%%%%%%%}/g" |  sed "s/\&/{%%%%}/g" | sed "s/:/{%%%}/g" | sed "s/　/ /g" | sed "s/ /,/g" \
+    | php -r "echo preg_quote(file_get_contents('php://stdin'));"`
   fi
 fi
 
@@ -83,7 +85,6 @@ fi
 #  Preprocedure
 # -----------------
 if [ "$filter_table" ];then
-  filter_table=`echo $filter_table | php -r "echo preg_quote(file_get_contents('php://stdin'));"`
   line_num=`$DATA_SHELL databox:$databox command:show_all[filter=${filter_table}][keys=$keys] format:none | wc -l`
 
 elif [ "$sort_col" ];then
