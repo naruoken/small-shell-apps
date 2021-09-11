a17aae80be906691761c012741b790aa1a6647b9
Ken Naruo <ken,naruo@gmail.com>
2021-09-11 01:13:42 +0000

    update 20210911

---
+++ team_app/bin/inquiry_get.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+# load query string param
+for param in `echo $@`
+do
+
+  if [[ $param == session:* ]]; then
+    session=`echo $param | awk -F":" '{print $2}'`
+  fi
+
+  if [[ $param == pin:* ]]; then
+    pin=`echo $param | awk -F":" '{print $2}'`
+  fi
+
+  if [[ $param == user_name:* ]]; then
+    user_name=`echo $param | awk -F":" '{print $2}'`
+  fi
+
+  if [[ $param == id:* ]]; then
+    id=`echo $param | awk -F":" '{print $2}'`
+  fi
+
+done
+
+# load small-shell path
+. ../descriptor/.small_shell_path
+
+if [ ! "$id"  ];then
+  id="new"
+fi
+
+if [ ! -d ../tmp/$session ];then
+  mkdir ../tmp/$session
+fi
+
+# SET BASE_COMMAND
+META="sudo -u small-shell ${small_shell_path}/bin/meta"
+DATA_SHELL="sudo -u small-shell ${small_shell_path}/bin/DATA_shell session:$session pin:$pin app:inquiry"
+
+if [ $id = "new" ];then
+
+  # gen reqd/write form #new
+  $DATA_SHELL databox:inquiries action:get id:$id keys:user_name,email,type,inquiry format:html_tag > ../tmp/$session/dataset
+
+else
+
+  # gen read only contents
+  #$DATA_SHELL databox:inquiries action:get id:$id keys:user_name,email,type format:none > ../tmp/$session/dataset.0.1
+  #cat ../tmp/$session/dataset.0.1 | sed "s/^/<li><label>/g" | sed "s/:/<\/label><p>/g" | sed "s/$/<\/p><\/li>/g" \
+  #| sed "s/<p><\/p>/<p>-<\/p>/g" >> ../tmp/$session/dataset
+
+  # gen interactive contents
+  $DATA_SHELL databox:inquiries action:merge.get id:$id key:inquiry >> ../tmp/$session/dataset
+  name=`$DATA_SHELL databox:inquiries action:get id:$id key:user_name format:none | awk -F ":" '{print $2}'`
+  email=`$DATA_SHELL databox:inquiries action:get id:$id key:email format:none`
+  type=`$DATA_SHELL databox:inquiries action:get id:$id key:type format:none`
+
+  # gen create history
+  $DATA_SHELL databox:inquiries action:get type:log id:$id format:none | head -1  > ../tmp/$session/history
+  echo "user:$name" >> ../tmp/$session/history
+  echo "$email" >> ../tmp/$session/history
+  echo "$type" >> ../tmp/$session/history
+
+fi
+
+# render HTML
+if [ "$id" = "new" ];then
+  cat ../descriptor/inquiry_new.html.def | sed "s/^ *</</g" \
+  | sed "/%%dataset/r ../tmp/$session/dataset" \
+  | sed "s/%%dataset//g"\
+  | sed "s/%%id/$id/g" \
+  | sed "s/%%params/session=$session\&pin=$pin/g"
+else
+  cat ../descriptor/inquiry_get.html.def | sed "s/^ *</</g" \
+  | sed "/%%dataset/r ../tmp/$session/dataset" \
+  | sed "s/%%dataset//g"\
+  | sed "/%%history/r ../tmp/$session/history" \
+  | sed "s/%%history//g"\
+  | sed "s/%%user_name/$name/g"\
+  | sed "s/%%id/$id/g" \
+  | sed "s/%%params/session=$session\&pin=$pin/g"
+fi
+
+if [ "$session" ];then
+  rm -rf ../tmp/$session
+fi
+
+exit 0
---
+++ team_app/deploy.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+WHOAMI=`whoami`
+if [ ! "$WHOAMI" = "root" ];then
+  echo "error: user must be root"
+  exit 1
+fi
+
+echo -n "small-shell root (/usr/local/small-shell): "
+read ROOT
+
+echo $ROOT
+
+if [ ! "$ROOT" ];then
+  ROOT=/usr/local/small-shell
+fi
+
+if [ ! -d $ROOT ];then
+  echo "error: there is no directory $ROOT"
+  exit 1
+fi
+
+# load web base
+. $ROOT/web/base
+
+if [ ! "$cgidir" ];then
+  echo "error: please gen base app first #${root}/adm/gen -app"
+  exit 1
+fi
+
+# deploy script to production env
+for src in `ls ./cgi-bin | xargs basename -a`
+do
+  cp ./cgi-bin/$src $cgidir/$src
+  chown $cgiusr:$cgiusr $cgidir/$src
+  chmod 755 $cgidir/$src
+done
+
+for src in `ls ./bin | xargs basename -a`
+do
+  cp ./bin/$src $cgidir/../bin/$src
+  chown $cgiusr:$cgiusr $cgidir/../bin/$src
+  chmod 755 $cgidir/../bin/$src
+done
+
+rand=$RANDOM
+for src in `ls ./descriptor | grep -v common_parts | xargs basename -a`
+do
+  cat ./descriptor/$src | sed "s/%%rand/$rand/g" > $cgidir/../descriptor/$src
+  chmod 755 $cgidir/../descriptor/$src
+done
+
+for src in `ls ./descriptor/common_parts | xargs basename -a`
+do
+  cat ./descriptor/common_parts/$src | sed "s/%%rand/$rand/g" > $cgidir/../descriptor/common_parts/$src
+  chmod 755 $cgidir/../descriptor/common_parts/$src
+done
+
+# create authkey for inquiry form
+app=inquiry
+app_user_name=${app}.app
+app_user_id=`echo "$app_user_name" | sha256sum | awk '{print $1}'`
+
+if [ ! -d $ROOT/users/${app}.${app_user_id} ];then
+  mkdir $ROOT/users/${app}.${app_user_id}
+fi
+echo "$app_user_name" > $ROOT/users/${app}.${app_user_id}/user_name
+echo "permission=rw" > $ROOT/users/${app}.${app_user_id}/.attr.tmp
+cat $ROOT/users/${app}.${app_user_id}/.attr.tmp > $ROOT/users/${app}.${app_user_id}/attr
+chown small-shell:small-shell $ROOT/users/${app}.${app_user_id}/attr
+
+which openssl  >/dev/null 2>&1
+if [ $? -eq 0 ];then
+  hash_gen_key=`openssl rand -hex 20`
+else
+  hash_gen_key="${RANDOM}.${RANDOM}.${RANDOM}.${RANDOM}.${RANDOM}"
+fi
+
+hash=`echo "${app}:${app_user_name}:${hash_gen_key}" | sha256sum | awk '{print $1}'`
+echo "$hash" > $ROOT/users/${app}.${app_user_id}/hash
+chown -R small-shell:small-shell $ROOT/users/${app}.${app_user_id}
+chmod 700 $ROOT/users/${app}.${app_user_id}/hash
+authkey=`echo "${app_user_name}:${hash_gen_key}" | base64 -w 0`
+
+# update form APP
+cat ./cgi-bin/inquiry | sed "s/%%authkey/$authkey/g" >  $cgidir/inquiry
+
+# create databox
+for src in `ls ./def | xargs basename -a`
+do
+  $ROOT/util/scripts/bat_gen.sh ./def/$src 
+done
+
+clear
+echo "-------------------------------------------------------------------------"
+echo "Team APP deployment has been done, please create APP user."
+echo "#sudo $ROOT/adm/ops add.usr:\$user app:team"
+echo "-------------------------------------------------------------------------"
+
+exit 0
