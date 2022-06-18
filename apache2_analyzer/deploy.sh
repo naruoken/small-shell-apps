8ecd8e34d3bc2cba6f15e7ad5e8a8b7fbdd8376f
Ken Naruo <ken.naruo@gmail.com>
2022-06-18 03:30:05 +0000

    release apache2 log analyzer

---
+++ apache2_analyzer/deploy.sh
@@ -0,0 +1,120 @@
+#!/bin/bash
+
+WHOAMI=`whoami`
+if [ ! "$WHOAMI" = "root" ];then
+  echo "error: user must be root"
+  exit 1
+fi
+
+echo -n "target domain or IP addr: "
+read domain
+
+if [ ! "$domain" ];then
+  echo "error: please input domain or IP addr as target of analytics"
+  exit 1
+fi
+
+echo -n "small-shell root (/usr/local/small-shell): "
+read ROOT
+
+if [ ! "$ROOT" ];then
+  ROOT=/usr/local/small-shell
+fi
+
+# log access permission check
+echo -n "apache2 log dir (/var/log/apache2): "
+read log_dir
+
+if [ ! "$log_dir" ];then
+  log_dir=/var/log/apache2
+fi
+
+if [ ! -d "$log_dir" ];then
+  echo "error: there is no $log_dir"
+  exit 1
+fi
+
+sudo -u small-shell ls /var/log/apache2/access.log >/dev/null 2>&1
+if [ ! $? -eq 0 ];then
+  echo "please add permission to the log dir, exit 1.."
+  echo "command sugesstion is \"usermod -aG adm small-shell\""
+  exit 1
+fi
+
+# change permission of sys user
+chk_permission=`$ROOT/bin/meta get.attr:sys`
+if [ ! "$chk_permission" = "rw" ];then
+  echo -n "Is it OK to add read and write permission to sys user ? (yes | no): "
+  read ANSWER
+  if [ "$ANSWER" = "yes" ];then
+    $ROOT/adm/ops set.attr:sys{rw}
+  else
+    echo "analyzer will sys user and it must have write permission to databox, exit 1..."
+    exit 1
+  fi
+fi
+
+
+# pyshell check
+. $ROOT/util/pyshell/env
+$PYTHON --version
+if [ $? -eq 0 ];then
+  echo "pyshell should be ready"
+else
+  echo "please install python libraries refering to https://small-shell.org/python_tour/"
+  exit 1
+fi
+
+# whois check 
+which whois >/dev/null 2>&1
+if [ $? -eq 0 ];then
+  echo "whois command is working"
+else
+  echo "you must install whois command, exit 1.."
+  exit 1
+fi
+
+# db creation
+$ROOT/util/scripts/bat_gen.sh ./db.def
+
+# deploy analyzer to util/scripts
+cat ./scripts/apache2_log_analyzer.sh  | sed "s#%%log_dir#$log_dir#g" > $ROOT/util/scripts/apache2_log_analyzer.sh
+chmod 755 $ROOT/util/scripts/apache2_log_analyzer.sh
+
+# job copy and enable 
+for job in `ls ./jobs`
+do
+  cat ./jobs/$job | sed "s/%%domain/$domain/g" > $ROOT/util/e-cron/def/$job
+done
+
+chmod 755 $ROOT/util/e-cron/def/log_analyzer.def
+chmod 755 $ROOT/util/e-cron/def/pv_statistics.def
+chmod 755 $ROOT/util/e-cron/def/uniq_statistics.def
+
+sudo -u small-shell $ROOT/bin/e-cron enable.log_analyzer
+if [ ! $? -eq 0 ];then
+  echo "error: something is wrong.failed to enable log_analyzer job"
+  exit 1
+fi
+
+sudo -u small-shell $ROOT/bin/e-cron enable.pv_statistics
+if [ ! $? -eq 0 ];then
+  echo "error: something is wrong.failed to enable pv_statistics job"
+  exit 1
+fi
+
+sudo -u small-shell $ROOT/bin/e-cron enable.uniq_statistics
+if [ ! $? -eq 0 ];then
+  echo "error: something is wrong.failed to enable uniq_statistics job"
+  exit 1
+fi
+
+# Note
+echo "--------------------------------------------------------------------------"
+echo "apache2 log analyzer is successfully deployed"
+echo "--------------------------------------------------------------------------"
+echo "1. analyzer will analyze access.log.1 once it's made by anacron on 00:00"
+echo "2. analytics will be done about 1 day ago log"
+echo "3. you can check graph and data on Base APP"
+
+exit 0
---
+++ apache2_analyzer/scripts/apache2_log_analyzer.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+#########################################################
+# :usage
+# apache2_log_analyer $domain_name
+#########################################################
+
+# load param
+domain=$1
+log="/var/log/apache2/access.log.1"
+SCRIPT_DIR=`dirname $0`
+. ${SCRIPT_DIR}/../../global.conf
+tmp="${SCRIPT_DIR}/tmp"
+date=`date +%Y-%m-%d --date '1 day ago'`
+
+# load authkey
+. ${SCRIPT_DIR}/.authkey
+
+if [ ! "$domain" ];then
+  echo "error: domain is null"
+  exit 1
+fi
+
+# command check
+which whois >/dev/null 2>&1
+if [  ! $? -eq 0 ];then
+  echo "error: please insetall whois command"
+  exit 1
+fi
+
+# analytics
+total_access_num=`cat $log | grep $domain | wc -l`
+cat $log | grep $domain | awk '{print $1}' | sort | uniq > ${tmp}/uniq_access.tmp
+uniq_access_num=`cat ${tmp}/uniq_access.tmp | wc -l`
+
+echo "#guess country code" >  ${tmp}/whois_dump
+echo "target:$log"
+echo "total_access:$total_access_num"
+echo "uniq_access:$uniq_access_num"
+echo "guess country code"
+echo "-----------------------------------------------------------------------------"
+while read client
+do
+
+  country_code=`whois $client | grep -i country | awk -F ":" '{print $2}' | sort | uniq \
+  | sed "s/ //g" | sed -z "s/\n/,/g" | sed "s/,$//g"`
+  echo "$client:$country_code"
+  echo "$client:$country_code" >> ${tmp}/whois_dump
+
+done < ${tmp}/uniq_access.tmp
+
+echo "-----------------------------------------------------------------------------"
+
+# dump data
+
+if [ ! -d ${tmp}/analyzer_dump ];then
+  mkdir ${tmp}/analyzer_dump
+fi
+
+echo $date > ${tmp}/analyzer_dump/date
+echo $total_access_num  > ${tmp}/analyzer_dump/pv
+echo $uniq_access_num  > ${tmp}/analyzer_dump/uniq_access
+cat ${tmp}/whois_dump  > ${tmp}/analyzer_dump/detail
+
+# push data to databox
+$ROOT/bin/DATA_shell databox:apache2_analyzer authkey:$authkey \
+action:set id:new keys:all input_dir:${tmp}/analyzer_dump
+
+exit 0
