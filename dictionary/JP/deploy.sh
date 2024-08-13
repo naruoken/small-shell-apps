a95286eca067cd61ce2699425bd387213034ef53
naruoken <ken.naruo@gmail.com>
2024-08-13 13:19:23 +0900

    release dictionary

---
+++ dictionary/JP/deploy.sh
@@ -0,0 +1,127 @@
+#!/bin/bash
+
+tmp_dir=./tmp
+echo -n "small-shell root (/usr/local/small-shell): "
+read ROOT
+
+if [ ! "$ROOT" ];then
+  ROOT=/usr/local/small-shell
+fi
+
+# loal global conf
+. $ROOT/global.conf
+
+# load web base
+. $ROOT/web/base
+
+WHOAMI=`whoami`
+if [ ! "$WHOAMI" = "root" ];then
+  echo "error: user must be root"
+  exit 1
+fi
+
+if [ ! -f ./keywords ];then
+  echo "please execute this script at `dirname $0`"
+  exit 1
+fi
+
+# update web/src
+cat ./keywords | grep -v "^#" > ${tmp_dir}/.dictionary
+while read line
+do
+  org="`echo $line | $AWK -F"{%%%%%%}" '{print $1}'`"
+  new="`echo $line | $AWK -F"{%%%%%%}" '{print $2}'`"
+
+  for dir in descriptor bin cgi-bin
+  do
+    grep -rl "${org}" $ROOT/web/src/$dir > .list.tmp
+    while read target
+    do
+      cat $target | $SED "s#${org}#${new}#g" > .target.new
+      cat .target.new > $target
+    done < .list.tmp
+  done
+done <  ${tmp_dir}/.dictionary
+
+# update www
+cat ./keywords | grep -v "^#" > ${tmp_dir}/.dictionary
+while read line
+do
+  org="`echo $line | $AWK -F"{%%%%%%}" '{print $1}'`"
+  new="`echo $line | $AWK -F"{%%%%%%}" '{print $2}'`"
+
+  for dir in descriptor bin cgi-bin
+  do
+    grep -rl "${org}" $www/$dir > .list.tmp
+    while read target
+    do
+      cat $target | $SED "s#${org}#${new}#g" > .target.new
+      cat .target.new > $target
+    done < .list.tmp
+  done
+done <  ${tmp_dir}/.dictionary 
+
+# update menu for Scratch APP
+portal=`grep "ScratchAPP:APP Portal" ./keywords | $SED "s/ScratchAPP:APP Portal{%%%%%%}//g"`
+table=`grep "ScratchAPP:Table" ./keywords | $SED "s/ScratchAPP:Table{%%%%%%}//g"`
+logout=`grep "ScratchAPP:Log Out" ./keywords | $SED "s/ScratchAPP:Log Out{%%%%%%}//g"`
+. $ROOT/util/scripts/.authkey
+permission=`$ROOT/bin/meta get.attr:sys`
+if [ "$permission" = "ro" ];then
+  $ROOT/adm/ops set.attr:sys{rw} > /dev/null 2>&1
+fi
+
+# update description and tmp/gen
+if [ -f ./tmplt.UI.md.def/description ];then
+  cat ./tmplt.UI.md.def/description > $ROOT/tmp/gen/.tmplt.UI.md.def/description
+fi
+if [ "$portal" -a "$table" ];then
+  for target in `ls $ROOT/tmp/gen/.tmplt.UI.md.def | xargs basename -a`
+  do
+      cat $ROOT/tmp/gen/.tmplt.UI.md.def/$target | $SED "s#APP Portal#${portal}#g" | $SED "s/Table/${table}/g" \
+      > .target.new
+      cat .target.new > $ROOT/tmp/gen/.tmplt.UI.md.def/$target
+  done
+fi
+if [ "$logout" ];then
+  cat $ROOT/adm/gen | $SED "s#Log Out#${logout}#g" > .target.new
+  cat .target.new > $ROOT/adm/gen
+fi
+
+for target in `ls ${www}/descriptor/common_parts/*_common_menu* | grep -v .org$ | xargs basename -a`
+do
+  app=`head -1 ${www}/descriptor/common_parts/${target} | $AWK -F "./" '{print $2}' | $AWK -F "?" '{print $1}'`
+  chk_team=`grep "# controller for Scratch APP #team" ${cgidir}/${app}`
+
+  if [ -f ${cgidir}/${app} -a ! -d ${tmp_dir}/${app} -a ! "${chk_team}" ];then       
+    # update UI.md.def
+    mkdir ${tmp_dir}/${app}
+
+    id=`sudo -u small-shell $ROOT/bin/DATA_shell authkey:${authkey} databox:${app}.UI.md.def action:get command:head_-1 format:none | awk -F "," '{print $1}'`
+    sudo -u small-shell $ROOT/bin/DATA_shell authkey:$authkey databox:${app}.UI.md.def action:get id:${id} key:righth format:none \
+    | $SED "s#APP Portal#${portal}#g" | $SED "s/Table/${table}/g" | $SED "s/Log Out/${logout}/g" \
+    | $SED "s/_%%enter_/\n/g" | $SED "s/righth://g" > ${tmp_dir}/${app}/righth
+    sudo -u small-shell $ROOT/bin/DATA_shell authkey:${authkey} databox:${app}.UI.md.def action:set id:${id} key:righth input_dir:${tmp_dir}/${app}
+
+    # update description
+    if [ -f ./tmplt.UI.md.def/description ];then
+      cat ./tmplt.UI.md.def/description | $SED "s/%%app/${app}/g" > ${tmp_dir}/${app}/description
+      sudo -u small-shell $ROOT/bin/DATA_shell authkey:${authkey} databox:${app}.UI.md.def action:set id:${id} key:description input_dir:${tmp_dir}/${app}
+    fi
+  fi
+done
+
+if [ "$permission" = "ro" ];then
+  $ROOT/adm/ops set.attr:sys{ro} > /dev/null 2>&1
+fi
+
+# update team
+if [ -f ./team/team_common_menu ];then
+  cat ./team/team_common_menu > ${www}/descriptor/common_parts/team_common_menu
+  cat ./team/team_common_menu > ../../team_app/descriptor/common_parts/team_common_menu
+fi
+
+rm -rf ${tmp_dir}/*
+
+echo "package deployment is completed"
+exit 0
