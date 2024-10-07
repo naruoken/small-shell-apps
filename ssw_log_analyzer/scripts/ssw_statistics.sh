e8ca8a2ce8cac0c109ff1ce4017d58e512fcab72
naruoken <ken.naruo@gmail.com>
2024-10-07 17:57:56 +0900

    improve ssw_log_analyzer

---
+++ ssw_log_analyzer/scripts/ssw_statistics.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# load param
+SCRIPT_DIR=`dirname $0`
+. ${SCRIPT_DIR}/../../global.conf
+date=`date +%Y-%m-%d --date '1 day ago'`
+
+# load authkey
+. ${SCRIPT_DIR}/.authkey
+
+# load web/base
+. ${ROOT}/web/base
+
+# make statistics
+$ROOT/util/scripts/sumup.sh type:line sumup_key:uniq_access frequency:daily title:pv set_time:"$date" \
+global_filter:"$date" databox:web_analyzer
+
+$ROOT/util/scripts/sumup.sh type:line sumup_key:attack frequency:daily title:attack set_time:"$date" \
+global_filter:"$date" databox:web_analyzer
+
+# replication check
+if [ "$cluster_server" ];then
+  if [ ! "$master" ];then
+    for replica in $replica_hosts
+    do
+      sudo -u small-shell scp -i /home/small-shell/.ssh/id_rsa $ROOT/util/statistics/rawdata/* small-shell@${replica}:$ROOT/util/statistics/rawdata/
+      sudo -u small-shell scp -i /home/small-shell/.ssh/id_rsa $ROOT/util/statistics/graph/* small-shell@${replica}:$ROOT/util/statistics/graph/
+    done
+  fi
+fi
+
+exit 0
