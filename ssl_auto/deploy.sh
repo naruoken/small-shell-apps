f949d08a6b9d89515a58debda8f800730b08de71
naruoken <ken.naruo@gmail.com>
2024-10-10 10:41:57 +0900

    improve ssl_autl/deploy.sh

--- ssl_auto/deploy.sh
+++ ssl_auto/deploy.sh
@@ -225,6 +225,7 @@ server {
            proxy_set_header Host \$host;
            proxy_set_header X-Real-IP \$remote_addr;
            proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+           client_max_body_size 4G;
 	}
 }
 
@@ -297,6 +298,7 @@ server {
            proxy_set_header Host \$host;
            proxy_set_header X-Real-IP \$remote_addr;
            proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+           client_max_body_size 4G;
 	}
 }
 EOF
